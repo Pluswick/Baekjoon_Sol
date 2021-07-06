6c4701d31d4510f82a595ac418fe0d9fcdfa75c9
DESKTOP-3GOTUVS\tjdgk <tjdgks0814@gmail.com>
2021-07-06 19:03:45 +0900

    Step-3(1).c
    Solutions for Baekjoon No.2739, 10950, 8393, 15552, 2741, 2742.
    백준 단계별 문제의 3단계 for문의 솔루션(1).

---
+++ Step-3(1).c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS
+/*#include<stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= 9; i++) {
+		printf("%d * %d = %d\n", n, i, n * i);
+	}
+} No.2739*/
+
+/*#include<stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+} No.10950*/
+
+/*#include<stdio.h>
+
+int main(void) {
+	int sum = 0;
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		sum = sum + i;
+	}
+	printf("%d", sum);
+} No.8393*/
+
+/*#include<stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+} No.15552*/   //C의 처리 속도가 다른 언어보다 빠르기 때문에 1초 미만으로 연산이 가능하다.
+
+/*#include<stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", i);
+	}
+} No.2741*/
+
+/*#include<stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	for (int i = n; i > 0; i--) {
+		printf("%d\n", i);
+	}
+} No.2742*/

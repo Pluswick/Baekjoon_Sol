1e99d0383048cef47b20483412a9e64d24992a76
DESKTOP-3GOTUVS\tjdgk <tjdgks0814@gmail.com>
2021-07-25 06:45:40 +0900

    Step-5(3).c
    Solutions for Baekjoon No.1546.
    백준 단계별 문제의 5단계 1차원 배열의 솔루션(3).

---
+++ Step-5(3).c
@@ -0,0 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+float Max(float n[], int size) {
+	float max = 0;
+	for (int i = 0; i < size; i++) {
+		if (max < n[i])
+			max = n[i];
+		else
+			;
+	}
+	return max;
+}
+
+void Forgery(float n[], int size, float maximum){
+	for (int i = 0; i < size; i++) {
+		n[i] = n[i] / maximum * 100;
+	}
+	return n;
+}
+
+float Average(float n[], int size) {
+	float sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += n[i];
+	}
+	float average = sum / size;
+
+	return average;
+}
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	float arr[1000];
+	for (int i = 0; i < n; i++) {
+		float num;
+		scanf("%f", &num);
+		arr[i] = num;
+	}
+	float max;
+	max = Max(arr, n);
+	Forgery(arr, n, max);
+	printf("%f", Average(arr, n));
+
+	return 0;
+}
